1474bf9cecac255929576b44d3f91ec68276e751
Gigio42 <gigio.42@gmail.com>
2025-06-20 00:36:18 -0300

    arruma tipagens pegas pelo linter, modif eslint novamente

--- backend(NestJS)/src/auth/auth.guard.ts
+++ backend(NestJS)/src/auth/auth.guard.ts
@@ -1,18 +1,27 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
+interface JwtPayload {
+  email: string;
+  id: number;
+}
+interface RequestWithUser extends Request {
+  user?: any;
+}
+
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest();
-    const token = request.headers.authorization?.replace('Bearer ', '');
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const authHeader = request.headers.get('authorization');
+    const token = authHeader?.replace('Bearer ', '');
     if (!token) {
       return false;
     }
     try {
-      const payload = this.jwtService.verify(token);
+      const payload = this.jwtService.verify<JwtPayload>(token);
       request.user = payload;
       return true;
     } catch (error) {
