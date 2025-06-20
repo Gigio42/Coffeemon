cb04927555ebcf5bc9d6ac1aedf487dc9129c31b
Gigio42 <gigio.42@gmail.com>
2025-06-20 04:29:10 -0300

    adiciona exemplos para criar ordem no swagger

--- backend(NestJS)/src/orders/dto/create-order.dto.ts
+++ backend(NestJS)/src/orders/dto/create-order.dto.ts
@@ -1,12 +1,26 @@
 import { IsNumber, IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderDto {
+  @ApiProperty({
+    description: 'Products in the order',
+    example: [
+      {
+        quantity: 2,
+        productId: 1,
+      },
+    ],
+  })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => CreateOrderItemDto)
   products: CreateOrderItemDto[];
 
+  @ApiProperty({
+    description: 'User ID who placed the order',
+    example: 1,
+  })
   @IsNumber({}, { message: 'Usuário é obrigatório' })
   userId: number;
 }
