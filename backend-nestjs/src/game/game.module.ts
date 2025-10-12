57cbf261859057217273cd3951b5c0801764b204
Gigio42 <gigio.42@gmail.com>
2025-10-12 03:49:53 -0300

    Refatora a estrutura do back para ser orientada a eventos + seed.ps1

--- backend-nestjs/src/game/game.module.ts
+++ backend-nestjs/src/game/game.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../ecommerce/users/users.module';
 import { BattlesModule } from './battles/battles.module';
@@ -6,17 +7,19 @@ import { CoffeemonModule } from './coffeemon/coffeemon.module';
 import { GameGateway } from './game.gateway';
 import { MatchmakingModule } from './matchmaking/matchmaking.module';
 import { PlayerModule } from './player/player.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot(),
     CoffeemonModule,
     PlayerModule,
     BattlesModule,
     MatchmakingModule,
     AuthModule,
     UsersModule,
+    NotificationsModule,
   ],
   providers: [GameGateway],
-  exports: [CoffeemonModule, PlayerModule, BattlesModule, MatchmakingModule],
 })
 export class GameModule {}
